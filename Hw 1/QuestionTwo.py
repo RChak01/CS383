f18f7e7c19a826f6711654f9a237790d737c2905
RajChak <rchak045@gmail.com>
2023-07-07 23:19:53 -0400

    static workign video

--- Hw 1/QuestionTwo.py
+++ Hw 1/QuestionTwo.py
@@ -1,69 +1,63 @@
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
 import os
-from numpy import linalg as LA
-import matplotlib.pyplot as plt
 import cv2
 
-DIR = 'yalefaces'
+toPath = 'yalefaces'
 SEPARATOR = os.sep
 data = []
 
 def standardize(arr):
-    mean = arr.mean()
-    std = arr.std(ddof=1)
+    mean = np.mean(arr)
+    std = np.std(arr, ddof=1)
     new_arr = (arr - mean) / std
     return new_arr
 
-def get_k_principal_components(data, k):
-    cov = np.cov(data.T)
-    d, v = LA.eig(cov)
-    indices = np.argsort(d)[::-1][:k]
-    max_vectors = v[:, indices]
+def PComponents(data, k):
+    mean = np.mean(data, axis=0)
+    centered_data = data - mean
+    _, _, v = np.linalg.svd(centered_data, full_matrices=False)
+    max_vectors = v[:k]
     return max_vectors
 
-files = os.listdir(DIR)
+files = os.listdir(toPath)
 subject_files = sorted([f for f in files if not f.endswith('.txt')])
 
-# Load data
 for filename in subject_files:
-    im = Image.open(os.path.join(DIR, filename)).resize((40, 40))
+    im = Image.open(os.path.join(toPath, filename)).resize((40, 40))
     arr = np.array(im).flatten()
     data.append(arr)
 
 data = np.array(data)
 std_data = standardize(data)
 
-# Perform PCA and reconstruction for each k
-k_values = range(data.shape[1])
+k_values = range(1, data.shape[0] + 1)
 video_frames = []
-for k in k_values:
-    w = get_k_principal_components(std_data, k)
 
-    # Project subject02.centerlight onto k principal components
-    subject_img_path = os.path.join(DIR, 'subject02.centerlight')
-    subject_img = Image.open(subject_img_path).resize((40, 40))
+reconstructed_images = np.zeros((len(k_values), 40, 40), dtype=np.uint8)
+
+for i, k in enumerate(k_values):
+    subject_img_path = subject_files[i]
+    subject_img = Image.open(os.path.join(toPath, subject_img_path)).resize((40, 40))
     subject_arr = np.array(subject_img).flatten()
     subject_std_arr = standardize(subject_arr)
-    z = np.dot(w.T, subject_std_arr)
 
-    # Reconstruct the person using k principal components
-    reconstructed_arr = np.dot(w, z)
-    # Undo standardization
-    reconstructed_arr = (reconstructed_arr * subject_std_arr.std(ddof=1)) + subject_std_arr.mean()
-    # Reshape to a 40x40 image/matrix
+    w = PComponents(std_data, k)
+    z = np.dot(subject_std_arr, w.T)
+
+    reconstructed_arr = np.sum(w * z[:, np.newaxis], axis=0) + np.mean(data, axis=0)
+    reconstructed_arr = (reconstructed_arr * std_data.std(ddof=1)) + std_data.mean()
+
     reconstructed_img = Image.fromarray(reconstructed_arr.astype(np.uint8).reshape((40, 40)))
 
-    # Superimpose current k value on the image
     img_with_text = reconstructed_img.copy()
     draw = ImageDraw.Draw(img_with_text)
     font = ImageFont.load_default()
     draw.text((0, 0), f'k={k}', font=font, fill=255)
 
-    # Append the frame to the video frames list
     video_frames.append(img_with_text)
+    reconstructed_images[k - 1] = reconstructed_arr.reshape((40, 40))
 
-# Save the frames as a video
 output_video_path = 'pca_reconstruction_video.mp4'
 frame_width, frame_height = video_frames[0].size
 fourcc = cv2.VideoWriter_fourcc(*"mp4v")
