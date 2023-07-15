c65ddea9f92f8308642b6291d25b11406e9f992a
RajChak <rchak045@gmail.com>
2023-07-14 23:41:09 -0400

    Week 3 / Homework 1 Done

---
+++ Hw 1/QuestionThree.py
@@ -0,0 +1,69 @@
+from PIL import Image, ImageDraw, ImageFont
+import numpy as np
+import os
+import cv2
+
+toPath = 'yalefaces'
+SEPARATOR = os.sep
+Matrix = []
+
+def standardize(arr):
+    mean = np.mean(arr)
+    std = np.std(arr, ddof=1)
+    new_arr = (arr - mean) / std
+    return new_arr
+
+def PComponents(Matrix, k):
+    mean = np.mean(Matrix, axis=0)
+    centered_data = Matrix - mean
+    _, _, v = np.linalg.svd(centered_data, full_matrices=False)
+    max_vectors = v[:k]
+    return max_vectors
+
+files = os.listdir(toPath)
+subject_files = sorted([f for f in files if not f.endswith('.txt')])
+
+for filename in subject_files:
+    im = Image.open(os.path.join(toPath, filename)).resize((40, 40))
+    arr = np.array(im).flatten()
+    Matrix.append(arr)
+
+Matrix = np.array(Matrix)
+std_data = standardize(Matrix)
+
+k_values = range(1, Matrix.shape[0] + 1)
+video_frames = []
+
+reconstructed_images = np.zeros((len(k_values), 40, 40), dtype=np.uint8)
+
+for i, k in enumerate(k_values):
+    subject_img_path = subject_files[i]
+    subject_img = Image.open(os.path.join(toPath, subject_img_path)).resize((40, 40))
+    subject_arr = np.array(subject_img).flatten()
+    subject_std_arr = standardize(subject_arr)
+
+    w = PComponents(std_data, k)
+    z = np.dot(subject_std_arr, w.T)
+
+    reconstructed_arr = np.sum(w * z[:, np.newaxis], axis=0) + np.mean(Matrix, axis=0)
+    reconstructed_arr = (reconstructed_arr * std_data.std(ddof=1)) + std_data.mean()
+
+    reconstructed_img = Image.fromarray(reconstructed_arr.astype(np.uint8).reshape((40, 40)))
+
+    img_with_text = reconstructed_img.copy()
+    draw = ImageDraw.Draw(img_with_text)
+    font = ImageFont.load_default()
+    draw.text((0, 0), f'k={k}', font=font, fill=255)
+
+    video_frames.append(img_with_text)
+    reconstructed_images[k - 1] = reconstructed_arr.reshape((40, 40))
+
+output_video_path = 'hw1.mp4'
+frame_width, frame_height = video_frames[0].size
+fourcc = cv2.VideoWriter_fourcc(*"mp4v")
+video_writer = cv2.VideoWriter(output_video_path, fourcc, 10.0, (frame_width, frame_height))
+
+for frame in video_frames:
+    video_writer.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
+
+video_writer.release()
--- Hw 1/QuestionTwo.py
+++ Hw 1/QuestionTwo.py
@@ -1,69 +1,47 @@
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image
 import numpy as np
 import os
-import cv2
+from numpy import linalg as LA
+import matplotlib.pyplot as plt
 
 toPath = 'yalefaces'
 SEPARATOR = os.sep
-data = []
+firstMatrix = []
+std_data = []
+
+
+def PComponents(data, k):
+    cov = np.cov(data.T)
+    d, v = LA.eig(cov)
+    indices = np.argsort(d)[::-1][:k]
+    max_vectors = v[:, indices]
+    return max_vectors
 
 def standardize(arr):
-    mean = np.mean(arr)
-    std = np.std(arr, ddof=1)
+    mean = arr.mean()
+    std = arr.std(ddof=1)
     new_arr = (arr - mean) / std
     return new_arr
 
-def PComponents(data, k):
-    mean = np.mean(data, axis=0)
-    centered_data = data - mean
-    _, _, v = np.linalg.svd(centered_data, full_matrices=False)
-    max_vectors = v[:k]
-    return max_vectors
+
 
 files = os.listdir(toPath)
-subject_files = sorted([f for f in files if not f.endswith('.txt')])
+for filename in files:
+    if not filename.endswith('.txt'):
+        im = Image.open(os.path.join(toPath, filename)).resize((40, 40))
+        arr = np.array(im).flatten()
+        firstMatrix.append(arr)
+        std_data.append(standardize(arr))
 
-for filename in subject_files:
-    im = Image.open(os.path.join(toPath, filename)).resize((40, 40))
-    arr = np.array(im).flatten()
-    data.append(arr)
+std_data = np.array(std_data)
 
-data = np.array(data)
-std_data = standardize(data)
+w = PComponents(std_data, 2)
+matrix_w = w.T
 
-k_values = range(1, data.shape[0] + 1)
-video_frames = []
+Z = np.dot(matrix_w, std_data.T)
 
-reconstructed_images = np.zeros((len(k_values), 40, 40), dtype=np.uint8)
-
-for i, k in enumerate(k_values):
-    subject_img_path = subject_files[i]
-    subject_img = Image.open(os.path.join(toPath, subject_img_path)).resize((40, 40))
-    subject_arr = np.array(subject_img).flatten()
-    subject_std_arr = standardize(subject_arr)
-
-    w = PComponents(std_data, k)
-    z = np.dot(subject_std_arr, w.T)
-
-    reconstructed_arr = np.sum(w * z[:, np.newaxis], axis=0) + np.mean(data, axis=0)
-    reconstructed_arr = (reconstructed_arr * std_data.std(ddof=1)) + std_data.mean()
-
-    reconstructed_img = Image.fromarray(reconstructed_arr.astype(np.uint8).reshape((40, 40)))
-
-    img_with_text = reconstructed_img.copy()
-    draw = ImageDraw.Draw(img_with_text)
-    font = ImageFont.load_default()
-    draw.text((0, 0), f'k={k}', font=font, fill=255)
-
-    video_frames.append(img_with_text)
-    reconstructed_images[k - 1] = reconstructed_arr.reshape((40, 40))
-
-output_video_path = 'pca_reconstruction_video.mp4'
-frame_width, frame_height = video_frames[0].size
-fourcc = cv2.VideoWriter_fourcc(*"mp4v")
-video_writer = cv2.VideoWriter(output_video_path, fourcc, 10.0, (frame_width, frame_height))
-
-for frame in video_frames:
-    video_writer.write(cv2.cvtColor(np.array(frame), cv2.COLOR_RGB2BGR))
-
-video_writer.release()
+plt.scatter(Z[0], Z[1])
+plt.xlabel('P.C 1')
+plt.ylabel('P.C 2')
+plt.title('Two PC')
+plt.show()
